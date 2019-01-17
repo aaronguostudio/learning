26b69f7e8bb617d6fd305c668d853935b60d5072
Aaron Guo <aaronguostudio@gmail.com>
2019-01-16 22:58:20 -0700

    Add react

---
+++ react/learn/a-z/life-cycle.js
@@ -0,0 +1,24 @@
+/*
+
+getDefaultProps
+getInitialState
+componentWillMount
+render 
+componentDidMount
+compoentWillReceiveProps
+shouldComponentUpdate
+componentWillUpdate
+componentDidUpdate
+componentWillUnmount
+
+
+Initial render -> constructor -> componentWillMount -> render -> componentDidMount -> componentWillReceiveProps -> shouldComponentUpdate ( this.setState ) -> componentWillUpdate ( this.forceUpdate) -> render -> componentDidUpdate -> componentWillUnmount
+
+
+
+
+
+
+*/
+
+
