e4426ea2266539eaf332de29081a9b3e07597be0
Aaron Guo <aaronguostudio@gmail.com>
2019-03-03 08:16:45 -0700

    add ml

---
+++ ai/learn/imooc/python-and-ml/ch05/playML/LinearRegression.py
@@ -0,0 +1,36 @@
+import numpy as np
+from .metrics import r2_score
+
+class LinearRegression:
+  def __init__(self):
+    self.coef_ = None
+    self.interception_ = None
+    self._theta_ = None
+
+  # normal equiation
+  def fit_normal(self, X_train, y_train):
+    assert X_train.shape[0] == y_train.shape[0], \
+      "the size of X_train must be equal to the size of y_train"
+    X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+    self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
+
+    self.interception_ = self._theta[0]
+    self.coef_ = self._theta[1:]
+
+    return self
+
+  def predict(self, X_predict):
+    assert self.interception_ is not None and self.coef_ is not None, \
+    "the feature number of X_predict must be equal to X_train"
+    assert X_predict.shape[1] == len(self.coef_), \
+      "the feature number of X_predict must be equal to X_train"
+
+    X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
+    return X_b.dot(self._theta)
+
+  def score(self, X_test, y_test):
+    y_predict = self.predict(X_test)
+    return r2_score(y_test, y_predict)
+
+  def __repr__(self):
+    return "LinearRegression()"
