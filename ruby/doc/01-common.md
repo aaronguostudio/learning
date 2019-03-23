40332dcddbf187c7aea14841413742994aa9da09
Aaron Guo <aaronguostudio@gmail.com>
2019-03-23 08:18:43 -0600

    add ruby

---
+++ ruby/doc/01-common.md
@@ -0,0 +1,4 @@
+# About Ruby
+- OOP
+- No primitives
+- Most everthing is object
---
+++ ruby/lynda/ch01/01-basic.rb
@@ -0,0 +1,17 @@
+# Using ruby from command line
+# ruby -e 'puts 123'
+
+# output
+puts 123
+puts 456
+puts 789
+
+
+# variable
+x = "X var"
+puts x
+
+# $var    global var
+# @@var   class var
+# @var    instance var
+# var     local var
