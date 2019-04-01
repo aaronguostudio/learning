8901a68270b1ccab73adfa8d65cf57f0c6ff5675
aaronguostudio <aaronguostudio@gmail.com>
2019-04-01 00:38:17 -0600

    Add basic dotnet code console app

---
+++ dotnet-core/pluralsight/build-dotnot-core-mvc-app/console-hello-world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace build_dotnot_core_mvc_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dotnet!");
+        }
+    }
+}
