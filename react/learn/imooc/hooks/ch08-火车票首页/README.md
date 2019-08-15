eb23deea1ce0f506711f744303db4f93d6b5320a
aaronguostudio <aaron.guo@payload.com>
2019-08-15 06:59:51 -0600

    update react hooks

--- react/learn/imooc/hooks/ch08-火车票首页/README.md
+++ react/learn/imooc/hooks/ch08-火车票首页/README.md
@@ -1,2 +1,15 @@
 # 数据结构与模块设计
 - refer to the code
+  - 这里面采用了集中构建数据结构的方式，值得学习。
+  - 构建之前，要先把 Mock 和模块都想清楚，大部分的逻辑聊熟于心之后再开始设计
+
+# 顶部导航栏
+- npm i prop-types --save
+  - 这个模块已经和 react 分开了，所以需要安装
+- 使用 useCallback 避免组件的重新渲染
+
+# 始发终到站
+- 理解如何传入 state 和 dispatch
+- 理解如何使用 useCallback 优化
+- 理解如何使用 useMemo 批量 bindActionCreators to dispatch
+  - 这样做是因为目前 react 还没有对 hooks 提供非常好的支持，以后可以关注更新
---
+++ react/learn/imooc/hooks/source/train-ticket/src/index/App.jsx
@@ -0,0 +1,140 @@
+import React, { useCallback, useMemo } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import './App.css';
+
+import Header from '../common/Header.jsx';
+import DepartDate from './DepartDate.jsx';
+import HighSpeed from './HighSpeed.jsx';
+import Journey from './Journey.jsx';
+import Submit from './Submit.jsx';
+
+import CitySelector from '../common/CitySelector.jsx';
+import DateSelector from '../common/DateSelector.jsx';
+
+import { h0 } from '../common/fp';
+
+import {
+    exchangeFromTo,
+    showCitySelector,
+    hideCitySelector,
+    fetchCityData,
+    setSelectedCity,
+    showDateSelector,
+    hideDateSelector,
+    setDepartDate,
+    toggleHighSpeed,
+} from './actions';
+
+function App(props) {
+    const {
+        from,
+        to,
+        isCitySelectorVisible,
+        isDateSelectorVisible,
+        cityData,
+        isLoadingCityData,
+        highSpeed,
+        dispatch,
+        departDate,
+    } = props;
+
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, []);
+
+    const cbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                exchangeFromTo,
+                showCitySelector,
+            },
+            dispatch
+        );
+    }, []);
+
+    const citySelectorCbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                onBack: hideCitySelector,
+                fetchCityData,
+                onSelect: setSelectedCity,
+            },
+            dispatch
+        );
+    }, []);
+
+    const departDateCbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                onClick: showDateSelector,
+            },
+            dispatch
+        );
+    }, []);
+
+    const dateSelectorCbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                onBack: hideDateSelector,
+            },
+            dispatch
+        );
+    }, []);
+
+    const highSpeedCbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                toggle: toggleHighSpeed,
+            },
+            dispatch
+        );
+    }, []);
+
+    const onSelectDate = useCallback(day => {
+        if (!day) {
+            return;
+        }
+
+        if (day < h0()) {
+            return;
+        }
+
+        dispatch(setDepartDate(day));
+        dispatch(hideDateSelector());
+    }, []);
+
+    return (
+        <div>
+            <div className="header-wrapper">
+                <Header title="火车票" onBack={onBack} />
+            </div>
+            <form action="./query.html" className="form">
+                <Journey from={from} to={to} {...cbs} />
+                <DepartDate time={departDate} {...departDateCbs} />
+                <HighSpeed highSpeed={highSpeed} {...highSpeedCbs} />
+                <Submit />
+            </form>
+            <CitySelector
+                show={isCitySelectorVisible}
+                cityData={cityData}
+                isLoading={isLoadingCityData}
+                {...citySelectorCbs}
+            />
+            <DateSelector
+                show={isDateSelectorVisible}
+                {...dateSelectorCbs}
+                onSelect={onSelectDate}
+            />
+        </div>
+    );
+}
+
+export default connect(
+    function mapStateToProps(state) {
+        return state;
+    },
+    function mapDispatchToProps(dispatch) {
+        return { dispatch };
+    }
+)(App);
