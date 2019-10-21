72076d704225ccbba1c5e99e32814c6784f07683
aaronguostudio <aaronguostudio@gmail.com>
2019-10-21 07:09:11 -0600

    start tensorflow

---
+++ tensorflow/doc/tutorials/start.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
