f2d57ace9e16dda03b9ffe053bed0b65b5506204
Aaron Guo <aaronguostudio@gmail.com>
2019-10-22 00:04:49 -0600

    TR-1 add tensorflow

--- tensorflow/doc/tutorials/start.py
+++ tensorflow/doc/tutorials/start.py
@@ -1,11 +1,32 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-# TensorFlow and tf.keras
+import ssl
 import tensorflow as tf
 from tensorflow import keras
-
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
+
+model = keras.Sequential([
+  keras.layers.Flatten(input_shape=(28, 28)),
+  keras.layers.Dense(128, activation="relu"),
+  keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested Acc: ", test_acc)
+
+
+# https://www.youtube.com/watch?v=6g4O5UOH304&t=3751s    56:57
