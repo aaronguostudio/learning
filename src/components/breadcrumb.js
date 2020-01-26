a3171a21abfaa3264e2f9a6d5494c3696444d368
aaronguostudio <aaronguostudio@gmail.com>
2020-01-25 23:53:27 -0700

    add responsive

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Breadcrumb } from 'antd'
+import { Breadcrumb, Icon } from 'antd'
 
 const Bread = ({ location }) => {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
       <Breadcrumb.Item>
+        <Icon type="home" />
+      </Breadcrumb.Item>
+      {/* <Breadcrumb.Item>
         <a href="">Application Center</a>
       </Breadcrumb.Item>
       <Breadcrumb.Item>
         <a href="">Application List</a>
       </Breadcrumb.Item>
-      <Breadcrumb.Item>An Application</Breadcrumb.Item>
+      <Breadcrumb.Item>An Application</Breadcrumb.Item> */}
     </Breadcrumb>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
 import Layout from "../components/layout"
 import 'antd/dist/antd.less'
 import '../styles/index.less'
 
 const IndexPage = () => (
   <Layout>
-    <p>Welcome to your new Gatsby blog with Markdown pages.</p>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Aaron Guo Blog</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width<% if (htmlWebpackPlugin.options.ctx.mode.cordova || htmlWebpackPlugin.options.ctx.mode.capacitor) { %>, viewport-fit=cover<% } %>"></meta>
+    </Helmet>
     <Link to="/blog/my-first-post/">Go to my first Markdown blog post</Link>
   </Layout>
 )
