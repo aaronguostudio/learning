102b346bfaed7fce52f02c25b5f5a6bfe72c6256
aaronguostudio <aaronguostudio@gmail.com>
2020-01-25 23:58:47 -0700

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => (
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width<% if (htmlWebpackPlugin.options.ctx.mode.cordova || htmlWebpackPlugin.options.ctx.mode.capacitor) { %>, viewport-fit=cover<% } %>"></meta>
     </Helmet>
-    <Link to="/blog/my-first-post/">Go to my first Markdown blog post</Link>
+    <h3>This blog is under development</h3>
   </Layout>
 )
 
